f5ed926e2ddb3f4437111b6ce3d3fec1dae340c9
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-04-17 23:53:42 +0530

    PW p1

---
+++ src/pwbasics1.ts
@@ -0,0 +1,19 @@
+//IIFE function: config file is not applicable
+
+import { chromium, Browser, Page } from "playwright";
+
+(async ()=>{
+//console.log("Hello World");
+//let browser:Browser = await chromium.launch({channel:'chromium',headless:false});
+let browser:Browser = await chromium.launch({channel:'chrome',headless:false});
+
+//let browser:Browser = await chromium.launch({channel:'msedge',headless:false});
+//let browser:Browser = await chromium.launch({channel:'brave',headless:false});
+
+let page: Page = await browser.newPage();
+await page.goto("https://www.flipkart.com");
+
+let title:string = await page.title();
+let url: string = page.url();
+//browser.close();
+})();
