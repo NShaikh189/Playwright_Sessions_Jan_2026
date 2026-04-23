81c7a3493a0fc0f186ab538a5d36a5a4a8463bd2
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-04-23 23:59:50 +0530

    code amend

--- tests/locator.spec.ts
+++ tests/locator.spec.ts
@@ -2,12 +2,14 @@
 import {test, Page} from '@playwright/test';
 
 test('register page', async ({page})=> {
+    let username =  page.getByTestId('username-input');
 //await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
 // page.getByRole('textbox', {name:'First Name'});
 await page.goto("https://naveenautomationlabs.com/opencart/ui/data-testid-page.html");
-
- await page.getByTestId('username-input').fill('hello wonder');
-await page.waitForTimeout(3000);
+//await username.highlight();
+await username.pressSequentially("Wonderland", {delay:300});
+// await username.fill('hello wonder');
+await page.waitForTimeout(5000);
 
 
 });
