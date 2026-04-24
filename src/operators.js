c5e712de5a29419dfc76e07746c0ef8b94e4b26d
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-04-24 23:38:47 +0530

    COde commit

--- src/operators.js
+++ src/operators.js
@@ -3,4 +3,37 @@ console.log(-1+1); //0
 console.log(-"1"+"1"); //-11
 console.log(-1+"1"); //-11
 
+console.log(1.5+2);
+
+let marks = 59;
+
+switch(marks)
+{
+    case marks>90
+    : console.log(">90");
+    break;
+    case marks>60: console.log(">60");
+    break;
+
+    case marks>30: console.log(">30");
+    break;
+    // default: console.log("Fail: <30");
+    // break;
+}
+
+/*
+switch(marks) {
+  case marks > 90:
+switch(marks) compares marks (a number) with each case
+marks > 90 evaluates to true/false
+So you're effectively doing:
+case true:
+
+👉 Now it becomes:
+
+switch(59) {
+  case true: // never matches
+
+So no case will match.
+*/
 
