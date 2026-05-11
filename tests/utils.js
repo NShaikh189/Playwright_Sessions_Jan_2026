0015f517061bee619317f37cd7522b00e87fac2d
Nayum Shaikh <nayumshaikh189@gmail.com>
2026-05-11 23:04:31 +0530

    util

---
+++ tests/utils.js
@@ -0,0 +1,19 @@
+
+let user = {
+    name: 'Tom',
+    age: 30
+};
+
+let flag = true;
+
+function add(a,b)
+{
+    return a+b;
+}
+
+export default function userInfo()
+{
+console.log ('welcome');
+}
+
+export {user, flag, add};
